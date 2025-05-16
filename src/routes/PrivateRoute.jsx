61605344ba92a86f536bd9c0ac9be79fc13ee3ca
Yousef Shaker Ardakani <yousofgamer1383@gmail.com>
2025-05-16 11:35:42 +0330

    updated

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("token"); // یا هر روش دیگه‌ای برای تشخیص لاگین
+  const isLoggedIn = localStorage.getItem("token");
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
