91f6bc789c97bfac10520f24e79627c2227d23a2
Yousef Shaker Ardakani <yousofgamer1383@gmail.com>
2025-06-08 15:32:09 +0330

    Alpha8.2

--- src/components/CreativeButton.jsx
+++ src/components/CreativeButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/button.css"; // Ensure you have the CSS for the button styles
 
-export default function CreativeButton ({text,type, onSubmitBtn}){
+export default function CreativeButton ({text,type, onSubmitBtn, color}){
+  const colorChange = color ? ` ${color}` : "";
   return (
     <button className="animated-button" type={type} onSubmit={onSubmitBtn}>
       <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
         <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
       </svg>
-      <span className="text">{text}</span>
+      <span className={"text" + colorChange}>{text}</span>
       <span className="circle"></span>
       <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
         <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import nfimg from "../assets/not-found.svg";
 import { Link } from "react-router-dom";
+import CreativeButton from "../components/CreativeButton";
 export default function PageNotFound() {
   return (
-    <div className="bg-gradient-to-r from-[#050A30] via-[#090979] to-[#050A30] text-white min-h-screen pt-24 pb-[120px] px-6 flex flex-col items-center justify-center">
-      {/* Your SVG below. Replace the example with your actual SVG code */}
-            <Link
-        to="/"
-        className="bg-black text-white px-6 py-3 rounded-full transition cursor-pointer"
-      >
-        Go Back Home
-      </Link>
-      <img src={nfimg} alt="" />
 
-    </div>
+      <div className="bg-white text-white min-h-screen pt-24 pb-[120px] px-6 flex flex-col items-center justify-center">
+        {/* Your SVG below. Replace the example with your actual SVG code */}
+        
+          <Link
+            to="/"
+            className=" px-6 py-3 rounded-full transition cursor-pointer mb-6"
+          >
+            <CreativeButton text={"Go to Home"} className=""></CreativeButton>
+          </Link>
+        
+        <img src={nfimg} alt="" />
+      </div>
   );
 }
